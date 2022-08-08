d92a6d983384ec755a56f35178064c9862e423e7
that_who_fighting_for_2D <101421494+HiikiToSS@users.noreply.github.com>
2022-08-08 19:23:49 +0000

    requirements.txt.append('dnspython','pymongo') докинул библиотек для бд крч

--- main.py
+++ main.py
@@ -3,26 +3,36 @@ import random
 from flask import Flask, request
 from telebot import types
 import telebot
+import pymongo
+from pymongo import MongoClient
 
 TOKEN = '5575632184:AAF4Wg5tNyb1eivb3WvTKZSZm6-XMKgs16c'
+CONNECTION_STRING = "mongodb+srv://hikki_bd:Ares_0377@cluster0.yv7ke.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+client = MongoClient(CONNECTION_STRING)
+
+db = client['db_for_roz'] # first_DB - название базы данных
+collection = db['userDB'] # создаём коллекцию
 
 bot = telebot.TeleBot(TOKEN)
 server = Flask(__name__)
 
 bot.send_message(1895572923, 'работаю')
 
-@bot.message_handler(content_types=['text'])
 def commands(message):
+  message.chat.type == "channel"
   if message.text == '/start':
     bot.send_message(message.chat.id, 'Введи /commands и /rand_num')
     print(1)
   elif message.text == '/commands':
     bot.send_message(message.from_user.id, '/ready /stat')
-  elif message.text == '/rand_num':
-    rand_num = random.choice(1, 50)
-    bot.send_messsage(message.chat.id, rand_num)
+  elif message.text == '/ready':
+    user = {
+      id : message.from_user.id
+    }
+    user = [user]
   else:
     bot.send_message(message.from_user.id, text = "Я ещё не нейронка чтобы отвечать на любые вопросы, введи /commands чтобы увидеть список команд")
+
   
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
@@ -33,7 +43,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://hikkibotik.herokuapp.com/' + TOKEN)
+    bot.set_webhook(url='https://rozbot01.herokuapp.com/' + TOKEN)
     return "!", 200
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
