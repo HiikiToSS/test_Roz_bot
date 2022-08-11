4349103776c39345226032a507e7c714625ef19d
HiikiToSS <101421494+HiikiToSS@users.noreply.github.com>
2022-08-11 13:24:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://dashboard.heroku.com/' + TOKEN)
+    bot.set_webhook(url='https://test-sth01.heroku.com/' + TOKEN)
     return "!", 200
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
