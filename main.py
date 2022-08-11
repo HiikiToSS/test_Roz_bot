a350394c4fad2dabc64a4782476b37196890d3c4
HiikiToSS <101421494+HiikiToSS@users.noreply.github.com>
2022-08-11 13:18:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
+from flask import Flask, request
 import os
 import random
 from telebot import types
 import telebot
 import pymongo
-from flask import Flask, request
 from pymongo import MongoClient
 
 server = Flask(__name__)
-TOKEN = '5424485104:AAGgOwaEL488DTeH6y3RLwxMEj70ziv6C5U'
+TOKEN = '5146045260:AAEoPSXOGulJbu3xA4qwGgrDUPFyxxJ0V0I'
 CONNECTION_STRING = "mongodb+srv://hikki_bd:Ares_0377@cluster0.yv7ke.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 client = MongoClient(CONNECTION_STRING)
 db = client['db_for_roz'] # first_DB - название базы данных
@@ -25,7 +25,7 @@ def commands(message):
     if message.text == '/start':
         bot.send_message(chat, 'Введи /commands и /rand_num')
     elif message.text == '/commands':
-        bot.send_message(from_user, '/ready /stat')
+        bot.send_message(from_user, 'Команды: \n /ready - для участия в розыгрыше \n /stat - кол-во участников')
     elif message.text == '/ready':
         user = {'id' : from_user}
         if collection.find_one(user) is None:
@@ -35,10 +35,11 @@ def commands(message):
             bot.send_message(chat, 'Ты уже участник')
     elif message.text == '/stat':
       all = list(collection.find())
-      bot.send_message(chat, 'Кол-во участников: ' + str(len(all)) + '\nТвой шанс на победу: ' + str(round(100/len(all), 2)) + '%')
+      bot.send_message(chat, 'Кол-во участников: ' + str(len(all)))
     else:
         bot.send_message(from_user, text = "Я ещё не нейронка чтобы отвечать на любые вопросы, введи /commands чтобы увидеть список команд")
-  
+
+
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
     json_string = request.get_data().decode('utf-8')
