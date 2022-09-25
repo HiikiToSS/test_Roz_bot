c2137865260eef345c7be31727ffdb3a0e1ba58e
HiikiToSS <101421494+HiikiToSS@users.noreply.github.com>
2022-09-25 14:18:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+'''from flask import Flask, request
 import os
 import random
 from telebot import types
@@ -55,4 +55,46 @@ def webhook():
     bot.set_webhook(url='https://test-sth01.heroku.com/' + TOKEN)
     return "!", 200
 if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))'''
+
+import os
+import random
+from telebot import types
+import telebot
+import pymongo
+from pymongo import MongoClient
+
+TOKEN = '5146045260:AAEoPSXOGulJbu3xA4qwGgrDUPFyxxJ0V0I' #AAEoPSXOGulJbu3xA4qwGgrDUPFyxxJ0V0I
+CONNECTION_STRING = "mongodb+srv://hikki_bd:Ares_0377@cluster0.yv7ke.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+client = MongoClient(CONNECTION_STRING)
+db = client['db_for_roz'] # first_DB - название базы данных
+collection = db['userDB'] # создаём коллекцию
+bot = telebot.TeleBot(TOKEN)
+
+bot.send_message(1895572923, 'работаю')
+
+def getIds(message):
+  return message.chat.id, message.from_user.id
+
+@bot.message_handler(content_types=['text'])
+def commands(message):
+    chat, from_user = getIds(message)
+    if message.text == '/start':
+        bot.send_message(chat, 'Введи /commands и /rand_num')
+    elif message.text == '/commands':
+        bot.send_message(from_user, 'Команды: \n /ready - для участия в розыгрыше \n /stat - кол-во участников')
+    elif message.text == '/ready':
+        user = {'id' : from_user}
+        if collection.find_one(user) is None:
+            bot.send_message(chat, 'Отлично, ты учавствуешь!')
+            collection.insert_one(user)
+        else:
+            bot.send_message(chat, 'Ты уже участник')
+    elif message.text == '/stat':
+      all = list(collection.find())
+      bot.send_message(chat, 'Кол-во участников: ' + str(len(all)))
+    else:
+        bot.send_message(from_user, text = "Я ещё не нейронка чтобы отвечать на любые вопросы, введи /commands чтобы увидеть список команд")
+
+
+bot.polling(none_stop=True)
