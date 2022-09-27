d230fd2928424404f081131059b00f39d82dd599
HiikiToSS <101421494+HiikiToSS@users.noreply.github.com>
2022-09-27 22:32:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -38,4 +38,16 @@ def commands(message):
     else:
         bot.send_message(from_user, text = "Я ещё не нейронка чтобы отвечать на любые вопросы, введи /commands чтобы увидеть список команд")
 
-bot.polling(none_stop=True)
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://dashboard.heroku.com/' + TOKEN)
+    return "!", 200
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
