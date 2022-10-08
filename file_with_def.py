2b42f17fb75b1183241be160a983cae4c58e337f
HiikiToSS <101421494+HiikiToSS@users.noreply.github.com>
2022-10-08 11:01:58 +0300

    Update file_with_def.py

--- file_with_def.py
+++ file_with_def.py
@@ -9,17 +9,18 @@ def statistic_about_user(from_user, bot, chat, all):
 def in_competition(bot, from_user, chat, collection):
     user = {'id' : from_user}
     if collection.find_one(user) is None:
-        bot.send_message(chat, 'Отлично, ты учавствуешь!')
+        bot.send_message(chat, 'Отлично, ты учавствуешь!\nЖди объявления результатов в @r34world')
         collection.insert_one(user)
     else:
         bot.send_message(chat, 'Ты уже участник')
 
 def every_user_chance(from_user, all, chat, bot):
-    if from_user == 1895572923 or from_user ==1046080555 or from_user == 1028594384:
+    if from_user == 1895572923 or from_user == 1046080555 or from_user == 1028594384:
         chance = 100/int(len(all))
         bot.send_message(chat, 'Шансы каждого участника = ' + str(round(chance, 1)) + '%')
     else: 
         bot.send_message(chat, 'Ты не явлешься админом')
+
 
 def comp(from_user, bot, all, input_message, chat, winners):
     def end_competition(message):
@@ -27,17 +28,23 @@ def comp(from_user, bot, all, input_message, chat, winners):
             bot.send_message(chat, 'Ты уверен?')
             bot.register_next_step_handler(message, get_first_access)
 
+    
     def get_first_access(message):
         if message.text == 'Да' or message.text == 'да':
             bot.send_message(chat, 'Точно?')
             bot.register_next_step_handler(message, get_second_access)
+
             
     def get_second_access(message):
         if message.text == 'Более чем' or message.text == 'более чем':
             winners = random.sample(all, 3)
             for winner in winners:
-                bot.send_message(winner["id"], 'Поздравляю, ты победил! Напиши @TheOutsider228 для получания награды')
+                bot.send_message(winner["id"], 'Поздравляю, ты победил! Напиши @TheOutsider228 для получания награды \n' + str(winner["id"]))
     end_competition(input_message)
+
     
 def get_all_commands(bot, from_user):
-    bot.send_message(from_user, 'Команды: \n /ready - для участия в розыгрыше \n /stat - кол-во участников \n /chance - шанс каждого из участников \n /end_roz - для окончания розыгрыша')
+    if from_user == 1895572923 or from_user == 1046080555 or from_user == 1028594384:
+        bot.send_message(from_user, 'Команды: \n /ready - для участия в розыгрыше \n /members - кол-во участников \n /chance - шанс каждого из участников \n /end_roz - окончание розыгрыша')
+    else:
+        bot.send_message(from_user, 'Команды: \n /ready - для участия в розыгрыше')
